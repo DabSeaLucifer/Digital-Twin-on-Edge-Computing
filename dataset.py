07e7fa08938b664b00e7fd6eb05def444a2db6ed
Sugeet Sood <100057945+SugeetSood@users.noreply.github.com>
2025-02-20 19:58:37 +0530

    Update dataset.py
    These normalisation values work better with cifar 10

--- dataset.py
+++ dataset.py
@@ -8,12 +8,17 @@ DATASET_PATH = "./data"
 def get_cifar10_datasets():
     """ Load CIFAR-10 dataset for FL training """
     transform = transforms.Compose([
+        transforms.RandomHorizontalFlip(),
+        transforms.RandomCrop(32, padding=4),
         transforms.ToTensor(),
-        transforms.Normalize((0.5,), (0.5,))
+        transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010))
     ])
 
     train_dataset = datasets.CIFAR10(root=DATASET_PATH, train=True, download=True, transform=transform)
-    test_dataset = datasets.CIFAR10(root=DATASET_PATH, train=False, download=True, transform=transform)
+    test_dataset = datasets.CIFAR10(root=DATASET_PATH, train=False, download=True, transform=transforms.Compose([
+        transforms.ToTensor(),
+        transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010))
+    ]))
 
     return train_dataset, test_dataset
 
